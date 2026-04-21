6a40d89063dc500fceeeef8f7efe8779849e3782
blackcoin2025 <bkcblackcoin@gmail.com>
2026-04-21 07:51:05 +0100

    les dernière mis ajour

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,28 +5,36 @@ import { FaHome, FaTasks, FaUserFriends, FaInfoCircle, FaWallet } from "react-ic
 
 const Footer = () => {
   return (
-    <div className="bottom-bar">
-      <Link to="/" className="nav-item">
-        <FaHome className="icon" />
-        <span className="nav-text">Home</span>
-      </Link>
-      <Link to="/tasks" className="nav-item">
-        <FaTasks className="icon" />
-        <span className="nav-text">Tasks</span>
-      </Link>
-      <Link to="/friends" className="nav-item">
-        <FaUserFriends className="icon" />
-        <span className="nav-text">Friends</span>
-      </Link>
-      <Link to="/info" className="nav-item">
-        <FaInfoCircle className="icon" />
-        <span className="nav-text">Info</span>
-      </Link>
-      <Link to="/wallet" className="nav-item">
-        <FaWallet className="icon" />
-        <span className="nav-text">Wallet</span>
-      </Link>
-    </div>
+    <footer className="footer">
+      <div className="footer-inner">
+
+        <Link to="/" className="nav-item">
+          <FaHome className="icon" />
+          <span className="nav-text">Home</span>
+        </Link>
+
+        <Link to="/tasks" className="nav-item">
+          <FaTasks className="icon" />
+          <span className="nav-text">Tasks</span>
+        </Link>
+
+        <Link to="/friends" className="nav-item">
+          <FaUserFriends className="icon" />
+          <span className="nav-text">Friends</span>
+        </Link>
+
+        <Link to="/info" className="nav-item">
+          <FaInfoCircle className="icon" />
+          <span className="nav-text">Info</span>
+        </Link>
+
+        <Link to="/wallet" className="nav-item">
+          <FaWallet className="icon" />
+          <span className="nav-text">Wallet</span>
+        </Link>
+
+      </div>
+    </footer>
   );
 };
 
